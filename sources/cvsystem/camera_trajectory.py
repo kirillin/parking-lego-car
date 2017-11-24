6c0b45d04560b9a565927f53618198c314ea32fa
kirillin <kaartemov@gmail.com>
2017-11-25 00:49:39 +0300

    Work version of CV for robot-car

--- sources/cvsystem/camera_trajectory.py
+++ sources/cvsystem/camera_trajectory.py
@@ -13,6 +13,7 @@ K = D_M / D_PX
 WIDTH_M = K * RESOLUTION[1]
 HEIGHT_M = K * RESOLUTION[0]
 
+
 class Marker:
 
     def __init__(self, cx, cy, cnt, color):
@@ -26,14 +27,17 @@ class Camera:
 
     def __init__(self, devNum):
         self.cap = cv2.VideoCapture(devNum)
-	
-	self.cap.set(3,640)
-	self.cap.set(4,480)
-	self.frame = None
+        self.cap.set(3, 640)
+        self.cap.set(4, 480)
+        self.frame = []
 
         # read standard of marker contour
         with np.load('circle.npz') as X:
             self.std_cnt_marker = [X[i] for i in X]
+
+    def init(self):
+        ret, frame = self.cap.read()
+        self.frame = frame
 
     @staticmethod
     def px2m(px):
@@ -53,18 +57,6 @@ class Camera:
         hsv = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2HSV)
 
 
-        # # blue circle
-        # lower_b = np.array([38, 142, 112])
-        # upper_b = np.array([168, 255, 253])
-        # mask_b = cv2.inRange(hsv, lower_b, upper_b)
-        #
-        # # red circle
-        # lower_r = np.array([0, 46, 255])
-        # upper_r = np.array([17, 255, 255])
-        # mask_r = cv2.inRange(hsv, lower_r, upper_r)
-
-        # mask = mask_b + mask_r
-
         # red-front
         lower = np.array([140, 30, 149])
         upper = np.array([188, 141, 197])
@@ -75,17 +67,15 @@ class Camera:
         upper = np.array([110, 242, 196])
         mask_rear = cv2.inRange(hsv, lower, upper)   # blue
 
-        # mask = mask_rear
         mask = mask_front + mask_rear
-        # cv2.imshow('mask', mask)
-
-        # # for both circles
-        # lower = np.array([135, 27, 47])
-        # upper = np.array([255, 176, 255])
-        # mask = cv2.inRange(hsv, lower, upper)
 
 
+        lower = np.array([0, 199, 49])
+        upper = np.array([164, 255, 255])
+        mask = cv2.inRange(hsv, lower, upper)
 
+
+        cv2.imshow('mask', mask)
         return mask
 
     def get_markers(self):
@@ -102,88 +92,48 @@ class Camera:
         frame = self.frame.copy()
         img_markers = cv2.bitwise_and(frame, frame, mask=mask)
 
-        __, cnts, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # __, cnts, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   # for opencv 3.
+        cnts, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # matching circles
-        cnts_on_car_markers = []
-        for cnt_std in self.std_cnt_marker:
-            for cnt in cnts:
-                ret = cv2.matchShapes(cnt, np.array(cnt_std, np.uint64), 1, 0)
-                if ret < 1:
-                    cnts_on_car_markers.append(cnt)
+        # matching circles. Don't use? becouse markers is tooooo smaaaall
+        # cnts_on_car_markers = []
+        # for cnt_std in self.std_cnt_marker:
+        #     for cnt in cnts:
+        #         ret = cv2.matchShapes(cnt, np.array(cnt_std, np.uint64), 1, 0)
+        #         if ret < 1:
+        #             cnts_on_car_markers.append(cnt)
 
         cnts_on_car_markers = cnts
-        cv2.drawContours(self.frame, cnts, -1, (220,220,0), 1)
+        # cv2.drawContours(self.frame, cnts, -1, (220,220,0), 1)
 
         # creation Marker objects
         marker_objs = []
         for cnt in cnts_on_car_markers:
             # print(cv2.contourArea(cnt))
-            if cv2.contourArea(cnt) > 45:
+            if cv2.contourArea(cnt) > 25:
                 M = cv2.moments(cnt)
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
                 color = tuple(map(int, frame[cy, cx]))  # color in center of contour
                 marker_objs.append(Marker(cx, cy, cnt, color))
+
         return marker_objs
 
     @staticmethod
-    def draw_traj_real_time(name, blank_image, traj):
+    def draw_traj_real_time(blank_image, traj):
         if len(traj['x']) > 4:
             for i in range(3, len(traj['x'])):
                 prev_x, prev_y = traj['x'][i-1], traj['y'][i-1]
                 x, y = traj['x'][i], traj['y'][i]
-                color = (0,0,0)
-                if name == 'red':
-                    color = (0,0,255)
-                elif name == 'blue':
-                    color = (255, 0, 0)
-                blank_image = cv2.line(blank_image, (int(prev_x), int(prev_y)), (int(x), int(y)), color, 1)
+                cv2.line(blank_image, (int(prev_x), int(prev_y)), (int(x), int(y)), (0,0,255), 1)
                 # print(prev_x, prev_y)
         cv2.imshow('trajectory', blank_image)
 
     @staticmethod
-    def draw_arrows(plt, frame, x, y, color):
-
-        scale = 1
-        aspace = 0.3  # good value for scale of 1
-        aspace *= scale
-
-        # r is the distance spanned between pairs of points
-        r = [0]
-        for i in range(1, len(x)):
-            dx = x[i] - x[i - 1]
-            dy = y[i] - y[i - 1]
-            r.append(np.sqrt(dx * dx + dy * dy))
-        r = np.array(r)
-        # rtot is a cumulative sum of r, it's used to save time
-        rtot = []
-        for i in range(len(r)):
-            rtot.append(r[0:i].sum())
-        rtot.append(r.sum())
-
-        arrowData = []  # will hold tuples of x,y,theta for each arrow
-        arrowPos = 0  # current point on walk along data
-        rcount = 1
-        while arrowPos < r.sum():
-            x1, x2 = x[rcount - 1], x[rcount]
-            y1, y2 = y[rcount - 1], y[rcount]
-            da = arrowPos - rtot[rcount]
-            theta = np.arctan2((x2 - x1), (y2 - y1))
-            ax = np.sin(theta) * da + x1
-            ay = np.cos(theta) * da + y1
-            arrowData.append((ax, ay, theta))
-            arrowPos += aspace
-            while arrowPos > rtot[rcount + 1]:
-                rcount += 1
-                if arrowPos > rtot[-1]:
-                    break
-
-        # could be done in above block if you want
-        for ax, ay, theta in arrowData:
-            # use aspace as a guide for size and length of things
-            # scaling factors were chosen by experimenting a bit
-            plt.arrow(ax, ay, np.sin(theta) * 0.2, np.cos(theta) * 0.2, head_width=0.1, head_length=0.2, color=color, width=0)
+    def draw_arrows(plt, frame, x, y, color, angle):
+        for i in range(len(x)):
+            plt.arrow(x[i], y[i], np.sin(angle[i] + np.pi/2) * 0.01, np.cos(angle[i] + np.pi/2) * 0.01,
+                      head_width=0.1, head_length=0.2, color=color, width=0)
 
         plt.plot(x, y, color=color)
 
